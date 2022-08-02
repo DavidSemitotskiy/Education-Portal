de480093253f4f3efe6730aa1eae3e1ee7927112
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-02 21:27:24 +0300

    add implementation of IUseRepository contract

---
+++ Portal/Portal.Infrastructure/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Portal.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Infrastructure
+{
+    internal class UserRepository : IUserRepository
+    {
+    }
+}
