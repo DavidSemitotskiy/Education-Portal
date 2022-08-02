b8f53624cae439d7f0f782e8ac35fbb487eb044f
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-02 21:08:58 +0300

    add contract for UserManager in Application layer

--- Portal/Portal.Domain/Models/User.cs
+++ Portal/Portal.Domain/Models/User.cs
@@ -8,5 +8,16 @@ namespace Portal.Domain.Models
 {
     public class User
     {
+        public Guid IdUser { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public int AccessLevel { get; set; }
+
+        public List<UserSkill>? Skills { get; set; }
     }
 }
