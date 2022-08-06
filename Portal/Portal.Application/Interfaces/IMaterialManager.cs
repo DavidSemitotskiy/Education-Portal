033b8b050febd22b1007082e7746902a3550a2ea
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 19:04:19 +0300

    add contract for MaterialManager

---
+++ Portal/Portal.Application/Interfaces/IMaterialManager.cs
@@ -0,0 +1,18 @@
+﻿using Portal.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Interfaces
+{
+    public interface IMaterialManager
+    {
+        IMaterialRepository MaterialRepository { get; set; }
+
+        void AddMaterial(Material material);
+
+        void DeleteMaterial(Material material);
+    }
+}
