74959761a88a648bb78cab0037f82ea18fec5bbf
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 12:02:45 +0300

    add first name and last name to user

--- Portal/Portal.Domain/Models/User.cs
+++ Portal/Portal.Domain/Models/User.cs
@@ -10,7 +10,9 @@ namespace Portal.Domain.Models
     {
         public Guid IdUser { get; set; }
 
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string Password { get; set; }
 
