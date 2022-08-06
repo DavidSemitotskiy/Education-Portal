73df7caf15cf1c416139dd8e9ffb2afb3adf9ab3
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 12:21:02 +0300

    add contract for MaterialRepository

---
+++ Portal/Portal.Domain/Interfaces/IMaterialRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain.Interfaces
+{
+    public interface IMaterialRepository
+    {
+        IEnumerable<Material> GetAllMaterials();
+
+        void Add(Material material);
+
+        void Delete(Material material);
+
+        void Update(Material material);
+
+        void SaveChanges();
+    }
+}
