3b9c11284f0098c2eb0296062e32de811451a0bb
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 19:26:34 +0300

    implement CourseManager

--- Portal/Portal.Application/CourseManager.cs
+++ Portal/Portal.Application/CourseManager.cs
@@ -1,4 +1,6 @@
 ﻿using Portal.Application.Interfaces;
+using Portal.Domain.Interfaces;
+using Portal.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,48 @@ namespace Portal.Application
 {
     internal class CourseManager : ICourseManager
     {
+        public CourseManager(ICourseRepository courseRepository)
+        {
+            CourseRepository = courseRepository ?? throw new ArgumentNullException("Repository can't be null");
+        }
+
+        public ICourseRepository CourseRepository { get; set; }
+
+        public void AddCourse(Course course)
+        {
+            if (course == null)
+            {
+                throw new ArgumentNullException("Course can't be null");
+            }
+
+            if (CourseRepository.Exists(course.Name, course.Description))
+            {
+                throw new ArgumentException("Course with this name and description already exists");
+            }
+
+            CourseRepository.Add(course);
+            //CourseRepository.SaveChanges();
+        }
+
+        public void DeleteCourse(Course course)
+        {
+            CourseRepository.Delete(course);
+            CourseRepository.SaveChanges();
+        }
+
+        public IEnumerable<Course> GetAvailableCourses(User user)
+        {
+            return CourseRepository.GetAllCourses().Where(course => course.AccessLevel <= user.AccessLevel);
+        }
+
+        public IEnumerable<Course> GetSubscribedCourses(User user)
+        {
+            return new List<Course>();
+        }
+
+        public IEnumerable<Course> GetOwnCourses(User user)
+        {
+            return CourseRepository.GetAllCourses().Where(course => course.Owner.IdUser == user.IdUser);
+        }
     }
 }
