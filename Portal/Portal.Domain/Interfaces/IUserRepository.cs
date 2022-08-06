8bcb8ac4e389bc61c47ae177a027eb4251388b38
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 12:14:32 +0300

    add DTOs into Domain and fill contract for UserRepository

--- Portal/Portal.Domain/Interfaces/IUserRepository.cs
+++ Portal/Portal.Domain/Interfaces/IUserRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Portal.Domain.DTOs;
+using Portal.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Portal.Domain.Interfaces
 {
-    public class IUserRepository
+    public interface IUserRepository
     {
+        IEnumerable<User> GetAllUsers();
+
+        void Add(User user);
+
+        bool Exists(string firstName, string lastName, string email);
+
+        User GetLogInUser(UserLoginDTO user);
     }
 }
