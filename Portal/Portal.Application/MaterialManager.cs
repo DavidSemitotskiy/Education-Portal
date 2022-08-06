42d193bf9a7cefef26091b4ca9bcf098699f6ef2
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 19:14:18 +0300

    implement MaterialManager

---
+++ Portal/Portal.Application/MaterialManager.cs
@@ -0,0 +1,37 @@
+﻿using Portal.Application.Interfaces;
+using Portal.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application
+{
+    internal class MaterialManager : IMaterialManager
+    {
+        public MaterialManager(IMaterialRepository materialRepository)
+        {
+            MaterialRepository = materialRepository ?? throw new ArgumentNullException("Repository can't be null");
+        }
+
+        public IMaterialRepository MaterialRepository { get; set; }
+
+        public IEnumerable<Material> GetAllMaterials()
+        {
+            return MaterialRepository.GetAllMaterials();
+        }
+
+        public void AddMaterial(Material material)
+        {
+            MaterialRepository.Add(material);
+            //MaterialRepository.SaveChanges();
+        }
+
+        public void DeleteMaterial(Material material)
+        {
+            MaterialRepository.Delete(material);
+            MaterialRepository.SaveChanges();
+        }
+    }
+}
