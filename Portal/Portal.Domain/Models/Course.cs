54b3d8bccacbde0b71045fe671d517a3ced3ba33
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 12:05:30 +0300

    add skills, subscribers, owner and override object's methods

--- Portal/Portal.Domain/Models/Course.cs
+++ Portal/Portal.Domain/Models/Course.cs
@@ -9,14 +9,35 @@ namespace Portal.Domain.Models
 {
     public class Course
     {
-        public int IdCourse { get; set; }
+        public Guid IdCourse { get; set; }
 
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        public User Owner { get; set; }
+
         public int AccessLevel { get; set; }
 
-        public List<Material>? IdCourses { get; set; }
+        public List<Material>? Materials { get; set; }
+
+        public List<User> Subscribers { get; set; }
+
+        public List<Skill> Skills { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Course other)
+            {
+                return GetHashCode() == other.GetHashCode();
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return IdCourse.GetHashCode() + Name.GetHashCode() + Description.GetHashCode();
+        }
     }
 }
