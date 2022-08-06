80c4b30ec7e9cc9c5178ff5f69fefce747a51691
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 13:04:21 +0300

    implement UserManager(registration, log in, log off) and createAccountService to get hash from password

---
+++ Portal/Portal.Application/AccountService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Application
+{
+    internal class AccountService
+    {
+        public string GetHashPassword(string password)
+        {
+            MD5 mD5 = MD5.Create();
+            var hashPassword = mD5.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hashPassword);
+        }
+    }
+}
--- Portal/Portal.Application/UserManager.cs
+++ Portal/Portal.Application/UserManager.cs
@@ -1,4 +1,7 @@
 ﻿using Portal.Application.Interfaces;
+using Portal.Domain.DTOs;
+using Portal.Domain.Interfaces;
+using Portal.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,74 @@ namespace Portal.Application
 {
     internal class UserManager : IUserManager
     {
+        public UserManager(IUserRepository userRepository)
+        {
+            UserRepository = userRepository ?? throw new ArgumentNullException("Repository can't be null");
+        }
+
+        public IUserRepository UserRepository { get; set; }
+
+        public User? CurrentUser { get; set; }
+
+        public void Register(UserRegisterDTO userRegister)
+        {
+            if (userRegister == null)
+            {
+                throw new ArgumentNullException("New User can't be null");
+            }
+
+            if (UserRepository.Exists(userRegister.FirstName, userRegister.LastName, userRegister.Email))
+            {
+                throw new ArgumentException("User with the same name already exists");
+            }
+
+            if (!userRegister.Password.Equals(userRegister.ConfirmPassword))
+            {
+                throw new ArgumentException("Confirm password doesn't match to Password");
+            }
+
+            AccountService accountService = new AccountService();
+            User user = new User
+            {
+                IdUser = Guid.NewGuid(),
+                FirstName = userRegister.FirstName,
+                LastName = userRegister.LastName,
+                Password = accountService.GetHashPassword(userRegister.Password),
+                Email = userRegister.Email,
+                AccessLevel = 0,
+                Skills = new List<UserSkill>()
+            };
+
+            UserRepository.Add(user);
+            CurrentUser = user;
+        }
+
+        public void LogIn(UserLoginDTO userLogin)
+        {
+            if (userLogin == null)
+            {
+                throw new ArgumentNullException("New User can't be null");
+            }
+
+            AccountService accountService = new AccountService();
+            userLogin.Password = accountService.GetHashPassword(userLogin.Password);
+            User logInUser = UserRepository.GetLogInUser(userLogin);
+            if (logInUser == null)
+            {
+                throw new Exception("User isn't registered");
+            }
+
+            CurrentUser = logInUser;
+        }
+
+        public void LogOff()
+        {
+            if (CurrentUser == null)
+            {
+                throw new ArgumentException("User isn't authorized");
+            }
+
+            CurrentUser = null;
+        }
     }
 }
