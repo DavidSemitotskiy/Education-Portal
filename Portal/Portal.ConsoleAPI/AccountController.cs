c3566b4c77074587cda9f747d934131130fbeccf
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 21:38:38 +0300

    add AccountController to register, log in and log out

---
+++ Portal/Portal.ConsoleAPI/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Portal.Application.Interfaces;
+using Portal.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.ConsoleAPI
+{
+    public class AccountController
+    {
+        public AccountController(IUserManager userManager)
+        {
+            UserManager = userManager ?? throw new ArgumentNullException("Manager can't be null");
+        }
+
+        public IUserManager UserManager { get; set; }
+
+        public void LogIn()
+        {
+            Console.Write("Input email: ");
+            string email = Console.ReadLine();
+            Console.Write("Input password: ");
+            string password = Console.ReadLine();
+            UserLoginDTO user = new UserLoginDTO
+            {
+                Email = email,
+                Password = password
+            };
+            UserManager.LogIn(user);
+        }
+
+        public void Register()
+        {
+            Console.Write("Input firstname: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Input lastname: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Input email: ");
+            string email = Console.ReadLine();
+            Console.Write("Input password: ");
+            string password = Console.ReadLine();
+            Console.Write("Confirm password: ");
+            string confirmPassword = Console.ReadLine();
+            UserRegisterDTO user = new UserRegisterDTO
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+            UserManager.Register(user);
+        }
+
+        public void LogOff()
+        {
+            UserManager.LogOff();
+        }
+    }
+}
