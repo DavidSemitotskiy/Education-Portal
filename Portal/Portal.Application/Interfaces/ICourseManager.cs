1616fdb8d5ffe06384cae506108bf027deba9923
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-08-06 19:19:33 +0300

    fill CourseManager contract

--- Portal/Portal.Application/Interfaces/ICourseManager.cs
+++ Portal/Portal.Application/Interfaces/ICourseManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Portal.Domain.Interfaces;
+using Portal.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace Portal.Application.Interfaces
 {
-    public class ICourseManager
+    public interface ICourseManager
     {
+        ICourseRepository CourseRepository { get; set; }
+
+        void AddCourse(Course course);
+
+        void DeleteCourse(Course course);
+
+        IEnumerable<Course> GetAvailableCourses(User user);
+
+        IEnumerable<Course> GetSubscribedCourses(User user);
+
+        IEnumerable<Course> GetOwnCourses(User user);
     }
 }
