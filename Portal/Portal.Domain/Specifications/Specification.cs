f9ebeafad8b29a6441008203a6fe952578253b3e
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-08 22:02:17 +0300

    add abstract specification class

---
+++ Portal/Portal.Domain/Specifications/Specification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain.Specifications
+{
+    public abstract class Specification<T>
+    {
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        public bool IsSatisfiedBy(T item)
+        {
+            var predicate = ToExpression().Compile();
+            return predicate(item);
+        }
+    }
+}
