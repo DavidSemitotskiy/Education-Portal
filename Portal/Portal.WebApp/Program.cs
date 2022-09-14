c99328c8987f5f48e9c44de37ce06b6c641b3fed
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-14 10:43:55 +0300

    done dependency injection

--- Portal/Portal.WebApp/Program.cs
+++ Portal/Portal.WebApp/Program.cs
@@ -1,6 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Portal.Application;
+using Portal.Application.Interfaces;
+using Portal.Domain.Interfaces;
+using Portal.Domain.Models;
+using Portal.EFInfrastructure;
+using Portal.EFInfrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<Context>();
+
+builder.Services.AddScoped<IEntityRepository<Course>, EntityRepository<Course>>();
+builder.Services.AddScoped<IEntityRepository<CourseSkill>, EntityRepository<CourseSkill>>();
+builder.Services.AddScoped<IEntityRepository<CourseState>, EntityRepository<CourseState>>();
+builder.Services.AddScoped<IEntityRepository<Material>, EntityRepository<Material>>();
+builder.Services.AddScoped<IEntityRepository<MaterialState>, EntityRepository<MaterialState>>();
+builder.Services.AddScoped<IEntityRepository<CourseSkill>, EntityRepository<CourseSkill>>();
+builder.Services.AddScoped<IEntityRepository<UserSkill>, EntityRepository<UserSkill>>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IMaterialStateManager, MaterialStateManager>();
+builder.Services.AddScoped<IUserSkillManager, UserSkillManager>();
+builder.Services.AddScoped<IApplicationUserManager, ApplicationUserManager>();
+builder.Services.AddScoped<ICourseStateManager, CourseStateManager>();
+builder.Services.AddScoped<ICourseManager, CourseManager>();
+builder.Services.AddScoped<ICourseSkillManager, CourseSkillManager>();
+builder.Services.AddScoped<IMaterialManager, MaterialManager>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -18,6 +51,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
