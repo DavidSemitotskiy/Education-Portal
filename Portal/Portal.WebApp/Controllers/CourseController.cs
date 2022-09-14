8709b76904f01443e41bd0190336b625e2791229
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-14 14:43:31 +0300

    add CourseController to operate with courses

---
+++ Portal/Portal.WebApp/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Portal.Application.Interfaces;
+
+namespace Portal.WebApp.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseManager _courseManager;
+
+        private readonly IApplicationUserManager _applicationUserManager;
+
+        public CourseController(ICourseManager courseManager, IApplicationUserManager applicationUserManager)
+        {
+            _courseManager = courseManager;
+            _applicationUserManager = applicationUserManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _applicationUserManager.UserRepository.FindByUserName(User.Identity.Name);
+            return View(await _courseManager.GetOwnCourses(user));
+        }
+    }
+}
