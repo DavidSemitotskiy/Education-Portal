c73ab17dcfbf0f6539b28d31d5ea23c7ed8f30e6
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-14 16:41:22 +0300

    add creating courses and show creating courses in CourseConstructor

--- Portal/Portal.WebApp/Controllers/CourseController.cs
+++ Portal/Portal.WebApp/Controllers/CourseController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Portal.Application.Interfaces;
+using Portal.Domain.Models;
+using Portal.WebApp.Models.CourseViewModels;
 
 namespace Portal.WebApp.Controllers
 {
@@ -27,6 +29,33 @@ namespace Portal.WebApp.Controllers
         {
             var user = await _applicationUserManager.UserRepository.FindByUserName(User.Identity.Name);
             return View(await _courseManager.GetCoursesInProgress(user));
+        }
+
+        public IActionResult CreateCourse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCourse(CreateCourseViewModel createdCourse)
+        {
+            if (ModelState.IsValid)
+            {
+                var courseToCreate = new Course
+                {
+                    Id = Guid.NewGuid(),
+                    Name = createdCourse?.Name,
+                    Description = createdCourse.Description,
+                    AccessLevel = createdCourse.AccessLevel,
+                    IsPublished = false,
+                    OwnerUser = User.Identity.Name
+                };
+                await _courseManager.AddCourse(courseToCreate);
+                await _courseManager.CourseRepository.SaveChanges();
+                return RedirectToAction("CourseConstructor");
+            }
+
+            return View(createdCourse);
         }
     }
 }
---
+++ Portal/Portal.WebApp/Models/CourseViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portal.WebApp.Models.CourseViewModels
+{
+    public class CreateCourseViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int AccessLevel { get; set; }
+    }
+}
