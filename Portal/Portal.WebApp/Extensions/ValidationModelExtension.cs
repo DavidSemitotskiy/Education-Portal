5f38c6445d20030410c56ed668f7cd1521d4249c
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-09-25 20:23:13 +0300

    add validation extension to validate model and put all errors to ModelState

---
+++ Portal/Portal.WebApp/Extensions/ValidationModelExtension.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Portal.Application.Validation;
+
+namespace Portal.WebApp.Extensions
+{
+    public static class ValidationModelExtension
+    {
+        public static async Task<bool> ValidateModel<TValidator, TTypeValidate>(this ErrorMessages<TValidator, TTypeValidate> errorMessages, TTypeValidate model, ModelStateDictionary? modelState)
+            where TValidator : AbstractValidator<TTypeValidate>, new()
+        {
+            var validator = new TValidator();
+            var resultValidation = await validator.ValidateAsync(model);
+            if (resultValidation.IsValid)
+            {
+                return true;
+            }
+
+            foreach (var error in resultValidation.Errors)
+            {
+                modelState?.AddModelError(string.Empty, error.ErrorMessage);
+            }
+
+            return false;
+        }
+    }
+}
